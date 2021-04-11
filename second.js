64dc7eba31b20e9d07e578641bd1508ae4136307
fghjjkl32 <fghjjkl32@naver.com>
2021-04-11 19:57:23 +0900

    about me : css,html fix (class naming)

---
+++ second.js
@@ -0,0 +1,14 @@
+const introduce_ment = document.querySelector(".introduce_ment");
+const rightBtn = document.querySelector(".chevron-right");
+const leftBtn = document.querySelector(".chevron-left");
+const boxes = document.querySelector(".")
+
+rightBtn.addEventListener('click', () => {
+    console.log("1");
+})
+
+leftBtn.addEventListener('click', () => {
+    console.log("2");
+})
+
+const d = [];
